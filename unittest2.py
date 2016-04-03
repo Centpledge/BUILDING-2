72579ac25d8659e0f08471508627f9b54728c0c7
Centpledge <apichit11@hotmail.com>
2016-04-04 01:08:11 +0700

    update

--- unittest2.py
+++ unittest2.py
@@ -2,6 +2,11 @@ def cube(n):
     return n**3
 def triarea(base,height):
     return base * height /2
+def evenOdd(i):
+    if i %2 ==0 :
+        return 'even'
+    else :
+        return 'odd'
 import unittest
 
 class myTest(unittest.TestCase):
@@ -13,6 +18,10 @@ class myTest(unittest.TestCase):
         self.assertEqual(triarea(10,10),50)
         self.assertEqual(triarea(0,0),0)
         self.assertEqual(triarea(5,10),25)
-
+    def test_evenOdd(self):
+        self.assertEqual(evenOdd(5),'odd')
+        self.assertEqual(evenOdd(-4),'even')
+        self.assertEqual(evenOdd(0),'even')
+        
 unittest.main()
         
